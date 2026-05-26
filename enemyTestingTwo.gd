bf7bfe138bea0aa3dcf11613f275e5250837cd6a
conor_felczak@horacemann.org <conorzak16@icloud.com>
2026-05-26 12:12:07 -0400

    inheritance

---
+++ enemyTestingTwo.gd
@@ -0,0 +1,12 @@
+extends Enemy
+
+# just how inheritance work I don't really need this honestly
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/enemy_testing.gd
+++ scripts/enemy_testing.gd
@@ -1,4 +1,5 @@
 extends CharacterBody3D
+class_name Enemy
 
 @export var leftArm := 10
 @export var rightArm := 10
